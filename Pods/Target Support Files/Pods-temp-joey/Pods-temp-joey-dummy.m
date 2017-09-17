b62916fe703e7c28771aafbfb763271f2209c9ff
Pathompong Chaisri <kiraboshi47@hotmail.com>
2017-09-17 23:03:58 +0700

    work with cmx

---
+++ Pods/Target Support Files/Pods-temp-joey/Pods-temp-joey-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_temp_joey : NSObject
+@end
+@implementation PodsDummy_Pods_temp_joey
+@end
--- temp-joey/ViewController.swift
+++ temp-joey/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let defaultManager: Alamofire.SessionManager = {
+        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+            "10.34.250.12": .disableEvaluation
+        ]
+
+        
+        return SessionManager(
+            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+        )
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        // Do any additional setup after loading the view, typically from a nib.
+//
+        let user = "dev"
+        let password = "dev12345"
+        
+
+        
+        var headers: HTTPHeaders = [:]
+        
+        if let authorizationHeader = Request.authorizationHeader(user: user, password: password) {
+            headers[authorizationHeader.key] = authorizationHeader.value
+        }
+        
+        
+
+        self.defaultManager.request("https://10.34.250.12/api/location/v2/clients/count", headers: headers).authenticate(user: user, password: password)
+
+            .responseString { response in
+                
+                debugPrint(response)
+                debugPrint(response.result)
+                
+//                            if let image = response.result.value {
+//                                self.imageView.image = image
+//
+//                            }
+                        }
     }
 
     override func didReceiveMemoryWarning() {
